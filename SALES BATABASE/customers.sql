f85dac125e2f94730c019d1896d35cb4b9af249d
Abdullahi Musa <abdullahimusa1524@gmail.com>
2025-08-17 23:17:19 +0000

    my sql assignment

---
+++ SALES BATABASE/customers.sql
@@ -0,0 +1,9 @@
+CREATE TABLE salesDB;
+
+CREATE TABLE customers(
+id INT,
+name TEXT,
+cart TEXT,
+price INT);
+INSERT INTO customers(id, name, cart, price)
+VALUES('1','Abdullahi-maker','pizza','20') 
